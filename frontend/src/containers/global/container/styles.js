031e7c984ce77e6489f1e0713bd86ae66ee808e6
daniel <daniellimafreita@gmail.com>
2024-08-15 21:37:50 -0300

    Update: responsividade na página de training

--- frontend/src/containers/global/container/styles.js
+++ frontend/src/containers/global/container/styles.js
@@ -4,10 +4,15 @@ export const Container = styled.div`
     display: flex;    
     height: 100vh;
     width: 100vw;
+    min-width: 1024px;
 `;
 export const Main = styled.main`
     flex: 1;
     width: 100%;
     height: 100%;
     padding: 40px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `;
--- frontend/src/pages/training/styles.js
+++ frontend/src/pages/training/styles.js
@@ -4,6 +4,9 @@ export const Container = styled.section`
     width: 100%;   
     display: flex;
     gap: 30px;
+    height: 100%;
+    align-items: center;
+    max-width: 1550px;
     
     > div {
         width: 50%;
