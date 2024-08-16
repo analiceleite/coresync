2d214532eea1d35c66e161b49537ca0717df8580
Analice Leite <analice.leite12@gmail.com>
2024-08-16 14:33:48 -0300

    feature: login e cadastro

---
+++ frontend/src/components/global/footer/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Footer = styled.footer`
+  width: 100%;
+  background-color: #01164d;
+  color: #f5f5f5;
+  text-align: center;
+  padding: 20px 0;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+`;
+
+export const Copyright = styled.p`
+  margin-top: 10px;
+  font-size: 12px;
+  color: #f5f5f5;
+  opacity: 0.8; /* Deixa o texto um pouco mais sutil */
+`;
--- frontend/src/containers/login/index.js
+++ frontend/src/containers/login/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MouseFollower from '../../components/mouse';
+import Footer from '../../components/global/footer';
 import Form from '../../components/login/form/form';
 import Logo from '../../assets/CoreSync-removebg.png'
 import * as S from './styles.js';
@@ -14,6 +15,7 @@ const Login = () => {
       <S.RightContainer>
         <S.Logo src={Logo} alt="CoreSync Logo" />
       </S.RightContainer>
+      <Footer />
     </S.LoginContainer>
   );
 };
