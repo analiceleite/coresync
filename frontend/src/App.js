aa8c721d839899f86b2d3b51396b3476a8b92c52
Analice Leite <analice.leite12@gmail.com>
2024-08-16 17:17:15 -0300

    upd url meeting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/training" element={<Training />} />
-          <Route path="/meet" element={<Meet />} />
+          <Route path="/meeting" element={<Meet />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
       </Router>
