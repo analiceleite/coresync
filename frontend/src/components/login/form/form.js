a5d360d4cfb1b7fca121dc183e3356ff1bf6367e
daniel <daniellimafreita@gmail.com>
2024-08-18 10:15:48 -0300

    Ref: Corrigindo tamanho do componente underline

--- frontend/src/components/login/form/form.js
+++ frontend/src/components/login/form/form.js
@@ -4,8 +4,10 @@ import { NavbarTraining, NavbarTrainingItem, Underline } from "../../../containe
 import PolkaDots from '../../../components/global/dots/index.js'
 import * as S from './styles.js';
 
-const Form = () => {
+const Form = () => { 
     const [itemActive, setItemActive] = useState(0);
+    const [underlineMove, setUnderlineMove] = useState("translateX(calc(100% / 4 * 0))");
+    const [underlineSize, setUnderlineSize] = useState("46px");
     const [passwordVisible, setPasswordVisible] = useState(false);
     const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
 
@@ -23,16 +25,24 @@ const Form = () => {
                 <NavbarTraining>
                     <ul>
                         <NavbarTrainingItem
-                            onClick={() => { setItemActive(0) }}
+                            onClick={() => { 
+                                setUnderlineMove("translateX(calc(100% / 4 * 0))");
+                                setUnderlineSize("46px"); 
+                                setItemActive(0);
+                            }}
                         >
                             Entrar
                         </NavbarTrainingItem>
                         <NavbarTrainingItem
-                            onClick={() => { setItemActive(1) }}
+                            onClick={() => { 
+                                setUnderlineMove("translateX(calc(100% / 4 * 4.8))");
+                                setUnderlineSize("73px");
+                                setItemActive(1);
+                            }}
                         >
                             Cadastrar
                         </NavbarTrainingItem>
-                        <Underline active={itemActive} scenario="login"></Underline>
+                        <Underline move={underlineMove} size={underlineSize} scenario="login"></Underline>
                     </ul>
                 </NavbarTraining>
                 <PolkaDots isSmall={true} />
