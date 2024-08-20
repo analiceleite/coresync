697cf3c00b0436661edcef14a2a24b268f6a1a92
daniel <daniellimafreita@gmail.com>
2024-08-20 14:55:24 -0300

    Merge: login + perfil

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,9 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path("perfil/", include("perfil.urls")),
-=======
     path('api/', include('authentication.api.urls')),
->>>>>>> 0782ce847ba6573b81e9b668ed6319cd63d78df7
 ]
