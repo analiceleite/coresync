72daf0a4729d3a407e54a6e887068703479feea2
Analice Leite <119526332+analiceleite@users.noreply.github.com>
2024-08-27 08:04:08 -0300

    register meet in progress

--- frontend/src/api/meet_api.js
+++ frontend/src/api/meet_api.js
@@ -1,23 +1,52 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = "http://localhost:8000";
 
-export const registerMeet = async (titulo, data, hora, descricao, participantes, duracao) => {
-    try {
-        const response = await axios.post(`${API_BASE_URL}/api/meet/`, {
-            titulo: titulo,
-            data: data,
-            hora: hora,
-            descricao: descricao,
-            duracao: duracao,
-            participantes: participantes,
-        }, {
-            headers: {
-                'Authorization': `Token ${localStorage.getItem('user_token')}`,
-            }
-        });
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
+export const registerMeet = async (
+  titulo,
+  data,
+  hora,
+  descricao,
+  participantes,
+  duracao
+) => {
+  try {
+    const response = await axios.post(
+      `${API_BASE_URL}/api/meet/`,
+      {
+        titulo: titulo,
+        data: data,
+        hora: hora,
+        descricao: descricao,
+        duracao: duracao,
+        participantes: participantes,
+      },
+      {
+        headers: {
+          Authorization: `Token ${localStorage.getItem("user_token")}`,
+        },
+      }
+    );
+    console.error("Erro ao registrar reunião:");
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getUsers = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/api/list-users/`, {
+      headers: {
+        Authorization: `Token ${localStorage.getItem("user_token")}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(
+      "Erro ao buscar usuários:",
+      error.response?.data || error.message
+    );
+    throw error;
+  }
 };
